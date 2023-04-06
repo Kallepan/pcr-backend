5f60971441c53b2741efb00837a83795699f809b
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-04-06 14:12:01 +0200

    project initialized

---
+++ launch.dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is used to launch the development environment for the
+
+export $(grep -v '^#' ./.dev.env | xargs)
+
+go run main.go
---
+++ psql/scripts/1_users.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION chkpass;
+
+CREATE TABLE users(
+    username VARCHAR (255) UNIQUE NOT NULL,
+    first_name VARCHAR (255) NOT NULL,
+    last_name VARCHAR (255) NOT NULL,
+    email VARCHAR (255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    user_id UUID PRIMARY KEY,
+
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+ALTER TABLE users ALTER COLUMN user_id SET DEFAULT uuid_generate_v4();

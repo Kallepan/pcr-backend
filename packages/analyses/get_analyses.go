33b55fababaa9796fba863bc914248bdf0647570
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-05-02 12:34:49 +0200

    filtering for analysis

--- packages/analyses/get_analyses.go
+++ packages/analyses/get_analyses.go
@@ -29,18 +29,69 @@ func GetAnalysis(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, &anlysis)
 }
 
+type AnalysisQueryParams struct {
+	Analyt   string `form:"analyt"`
+	Assay    string `form:"assay"`
+	Material string `form:"material"`
+
+	// Pagination
+	OrderBy string `form:"order_by"`
+	Limit   string `form:"limit"`
+}
+
+func getFilterString(query_params *AnalysisQueryParams) (string, []interface{}) {
+	var filters []string
+	var params []interface{}
+	if query_params.Analyt != "" {
+		filters = append(filters, "analyt = $3")
+		params = append(params, query_params.Analyt)
+	}
+	if query_params.Assay != "" {
+		filters = append(filters, "assay = $4")
+		params = append(params, query_params.Assay)
+	}
+	if query_params.Material != "" {
+		filters = append(filters, "material = $5")
+		params = append(params, query_params.Material)
+	}
+
+	query := ""
+	if len(filters) > 0 {
+		query += " WHERE "
+		for i, filter := range filters {
+			query += filter
+			if i != len(filters)-1 {
+				query += " AND "
+			}
+		}
+	}
+	println(query, params)
+
+	return query, params
+}
+
 func GetAllAnalyses(ctx *gin.Context) {
 	var analyses []models.Analysis
+	var query_params = AnalysisQueryParams{
+		OrderBy: "analysis_id",
+		Limit:   "100",
+	}
+	ctx.ShouldBindQuery(&query_params)
 
 	// Get all analyses
 	query :=
 		`
 		SELECT analysis_id,analyt,assay,material,ready_mix
 		FROM analyses
-		ORDER BY analysis_id ASC;
 		`
+	// Add filters
+	filter_string, filter_params := getFilterString(&query_params)
+	query += filter_string
 
-	rows, err := database.Instance.Query(query)
+	// Add pagination
+	query += ` ORDER BY $1 LIMIT $2;`
+	filter_params = append([]interface{}{query_params.OrderBy, query_params.Limit}, filter_params...)
+	rows, err := database.Instance.QueryContext(ctx, query, filter_params...)
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return

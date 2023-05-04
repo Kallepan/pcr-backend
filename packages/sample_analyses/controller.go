189abb1a8393077b77a8e797d9bf88cc37d35081
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-05-04 14:08:26 +0200

    delete sample analyses

--- packages/sample_analyses/controller.go
+++ packages/sample_analyses/controller.go
@@ -5,4 +5,5 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(router *gin.RouterGroup) {
 	router.POST("", AddAnalysisToSample)
 	router.PATCH("/:sample_id/:analysis_id", UpdateSampleAnalysis)
+	router.DELETE("/:sample_id/:analysis_id", DeleteSampleAnalysis)
 }

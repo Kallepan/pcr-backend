fbed055e8efb67311bfea4991242c5f7906257dd
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-06-29 11:28:43 +0200

    fixed migrations switchup

--- migrations/0003_add_trigger_functions_samplesanalyses.down.sql
+++ migrations/0003_add_trigger_functions_samplesanalyses.down.sql
@@ -1,23 +1,2 @@
-CREATE OR REPLACE FUNCTION update_position()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.run IS NOT NULL AND NEW.device IS NOT NULL THEN
-        IF NEW.position IS NULL THEN
-            NEW.position := (
-                SELECT COALESCE(MAX(position) + 1, 1)
-                FROM samplesanalyses
-                WHERE 
-                    DATE(created_at) = CURRENT_DATE AND
-                    position IS NOT NULL
-            );
-        END IF;
-    END IF;
-RETURN NEW;
-END;
-
-
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER update_position
-BEFORE UPDATE ON samplesanalyses
-FOR EACH ROW EXECUTE PROCEDURE update_position();
+DROP FUNCTION IF EXISTS update_position();
+DROP TRIGGER IF EXISTS update_position ON samplesanalyses;
--- migrations/0003_add_trigger_functions_samplesanalyses.up.sql
+++ migrations/0003_add_trigger_functions_samplesanalyses.up.sql
@@ -1,2 +1,23 @@
-DROP FUNCTION IF EXISTS update_position();
-DROP TRIGGER IF EXISTS update_position ON samplesanalyses;
+CREATE OR REPLACE FUNCTION update_position()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.run IS NOT NULL AND NEW.device IS NOT NULL THEN
+        IF NEW.position IS NULL THEN
+            NEW.position := (
+                SELECT COALESCE(MAX(position), 0) + 1
+                FROM samplesanalyses
+                WHERE 
+                    DATE(created_at) = CURRENT_DATE AND
+                    position IS NOT NULL
+            );
+        END IF;
+    END IF;
+RETURN NEW;
+END;
+
+
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_position
+BEFORE UPDATE ON samplesanalyses
+FOR EACH ROW EXECUTE PROCEDURE update_position();

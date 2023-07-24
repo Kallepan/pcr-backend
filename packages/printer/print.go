0b3320b678c046e92e68686a8cc42697e404f72d
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-07-24 10:37:00 +0200

    fix: printing disorderly

--- packages/printer/print.go
+++ packages/printer/print.go
@@ -23,9 +23,13 @@ A30,100,0,1,1,1,N,"%s"
 P1
 `
 
-type PrintRequest struct {
+type PrintRequestElement struct {
 	SampleId string `json:"sample_id" binding:"required"`
 	PanelId  string `json:"panel_id" binding:"required"`
+}
+
+type PrintRequest struct {
+	Elements []PrintRequestElement `json:"elements" binding:"required"`
 }
 
 type PrintData struct {
@@ -39,8 +43,8 @@ type PrintData struct {
 }
 
 func Print(ctx *gin.Context) {
+	// Connect to the printer
 	conn, err := net.Dial("tcp", printerAddress)
-
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
@@ -54,38 +58,42 @@ func Print(ctx *gin.Context) {
 		return
 	}
 
-	var printData PrintData
-	printData.SampleId = request.SampleId
-	printData.Panel = request.PanelId
-
 	// Query Sample to retrieve the inge name
-	query := `
-		SELECT position, run_date, samples.full_name, device, run
-		FROM samplespanels
-		LEFT JOIN samples ON samplespanels.sample_id = samples.sample_id
-		WHERE samplespanels.sample_id = $1 AND panel_id = $2
-		`
-	err = database.Instance.QueryRow(query, request.SampleId, request.PanelId).Scan(&printData.Position, &printData.Date, &printData.Name, &printData.Device, &printData.Run)
+	var labels []string
 
-	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		return
+	for _, element := range request.Elements {
+		var printData PrintData
+		query := `
+			SELECT position, run_date, samples.full_name, device, run
+			FROM samplespanels
+			LEFT JOIN samples ON samplespanels.sample_id = samples.sample_id
+			WHERE samplespanels.sample_id = $1 AND panel_id = $2
+		`
+		err = database.Instance.QueryRow(query, element.SampleId, element.PanelId).Scan(&printData.Position, &printData.Date, &printData.Name, &printData.Device, &printData.Run)
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		// Format Date
+		printData.Date = printData.Date[0:10]
+
+		// Generate the label string
+		label := fmt.Sprintf(label, printData.Position, element.SampleId, printData.Name, element.PanelId, printData.Device+printData.Run, printData.Date)
+		regex := regexp.MustCompile("[[:^ascii:]]")
+		label = regex.ReplaceAllString(label, "?")
+
+		labels = append(labels, label)
 	}
 
-	// Format Date and Sample
-	printData.Date = printData.Date[0:10]
-
-	// Replace all non-ASCII characters with a question mark
-	printString := fmt.Sprintf(label, printData.Position, printData.SampleId, printData.Name, printData.Panel, printData.Device+printData.Run, printData.Date)
-	regex := regexp.MustCompile("[[:^ascii:]]")
-	printString = regex.ReplaceAllString(printString, "?")
-
-	// Send the label to the printer
-	_, err = conn.Write([]byte(printString))
-
-	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		return
+	// If no error occurred, send the labels to the printer
+	for _, label := range labels {
+		_, err = conn.Write([]byte(label))
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
 	}
 
 	ctx.Status(http.StatusOK)
